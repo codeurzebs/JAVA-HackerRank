6d6956c5f84f5b004fc842b0619951eb3081415c
NGUENAZEBS <ngcodex@gmail.com>
2023-04-03 02:10:38 +0200

    Add files via upload

---
+++ src/main/java/hackerranksolution/Test.java
@@ -0,0 +1,11 @@
+package hackerranksolution;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+		//les solutions sont en cours bien venu sur la certif Hackerranck
+		
+	}
+
+}
