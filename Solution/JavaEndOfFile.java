ab2936f986b8c2c9019f592413a2eb302dd7486d
NGUENAZEBS <nguena.haupur@ictuniversity.edu.cm>
2023-04-08 19:17:53 +0200

    Add files via upload
    ZEBS JAVA CERTIFICATION

---
+++ Solution/JavaEndOfFile.java
@@ -0,0 +1,19 @@
+
+public class JavaEndOfFile {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = 1;
+		while (sc.hasNext()) {
+			System.out.println(i++ + " " + sc.nextLine());
+		}
+		sc.close();
+	}
+}
+/*
+*
+by @Codeur ZEBS 2023
+ICT-UNIVERSITY
+Java Developer
+NGcodeX
+*
+*/
---
+++ Solution/JavaIntToString.java
@@ -0,0 +1,13 @@
+
+public class JavaIntToString {
+	int n; //already decalared
+	String s=n+"";
+}
+/*
+*
+by @Codeur ZEBS 2023
+ICT-UNIVERSITY
+Java Developer
+NGcodeX
+*
+*/
